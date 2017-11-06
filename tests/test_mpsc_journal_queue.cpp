1be153fe7f581b1189a52be72d5f07210557dd0d
Jakob Leben <jakob.leben@gmail.com>
2017-11-06 00:39:04 -0800

    MPSC queue: Stress test

--- tests/test_mpsc_journal_queue.cpp
+++ tests/test_mpsc_journal_queue.cpp
@@ -37,9 +37,89 @@ static bool test()
     return test.success();
 }
 
+static bool stress_test()
+{
+    Testing::Test test;
+
+    MPSC_Journal_Queue<int> q(50);
+
+    atomic<bool> quit { false };
+
+    auto producer = ([&](bool first)
+    {
+        int v = 1;
+
+        while(!quit)
+        {
+            while(!quit)
+            {
+                //printf("%s %d\n", first ? "1: " : "2: ", v);
+                bool ok = q.push(first ? v : v + 1000);
+                if (ok)
+                    v = (v + 1) & 0xFF;
+                else
+                    break;
+            }
+
+            this_thread::yield();
+        }
+    });
+
+    thread p1(producer, true);
+    thread p2(producer, false);
+
+    bool work = true;
+    int v1 = 1;
+    int v2 = 1;
+    int v;
+
+    auto start = chrono::steady_clock::now();
+
+    while(work)
+    {
+        this_thread::yield();
+
+        work &= chrono::steady_clock::now() - start < chrono::seconds(5);
+
+        while(work && q.pop(v))
+        {
+            work &= chrono::steady_clock::now() - start < chrono::seconds(5);
+
+            if (v < 1000)
+            {
+                bool correct = v1 == v;
+                test.assert("Stream 1 = " + to_string(v)
+                            + ", expected " + to_string(v1),
+                            correct);
+                v1 = (v + 1) & 0xFF;
+                //printf("1: Next: %d\n", v1);
+                work &= correct;
+            }
+            else
+            {
+                v -= 1000;
+                bool correct = v2 == v;
+                test.assert("Stream 2 = " + to_string(v)
+                            + ", expected " + to_string(v2),
+                            correct);
+                v2 = (v + 1) & 0xFF;
+                //printf("2: Next: %d\n", v2);
+                work &= correct;
+            }
+        }
+    }
+
+    quit = true;
+    p1.join();
+    p2.join();
+
+    return test.success();
+}
+
 Testing::Test_Set mpsc_queue_tests()
 {
     return {
-        { "test", test }
+        { "test", test },
+        { "stress", stress_test },
     };
 }
