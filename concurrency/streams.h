0cd42557ab5db13770f50f5bddeda0c20153f643
Jakob Leben <jakob.leben@gmail.com>
2017-11-20 21:44:59 -0800

    Streams: Use lock-free set

--- concurrency/streams.h
+++ concurrency/streams.h
@@ -1,16 +1,50 @@
 #pragma once
 
 #include "mpsc_queue.h"
+#include "lockfree_set.h"
 
 #include <mutex>
 #include <list>
 #include <algorithm>
+#include <memory>
 
 namespace Reactive {
 
 using std::mutex;
 using std::lock_guard;
 using std::list;
+using std::shared_ptr;
+
+namespace Detail {
+
+template <typename T>
+struct Stream_Producer_Data;
+
+template <typename T>
+struct Stream_Consumer_Data;
+
+template <typename T>
+using Stream_Consumer_Data_Ptr = shared_ptr<Stream_Consumer_Data<T>>;
+
+template <typename T>
+using Stream_Producer_Data_Ptr = shared_ptr<Stream_Producer_Data<T>>;
+
+template <typename T>
+struct Stream_Producer_Data
+{
+    Set<Stream_Consumer_Data_Ptr<T>> sinks;
+};
+
+template <typename T>
+struct Stream_Consumer_Data
+{
+    Stream_Consumer_Data(int capacity): queue(capacity) {}
+
+    Set<Stream_Producer_Data_Ptr<T>> sources;
+    MPSC_Queue<T> queue;
+};
+
+}
 
 template <typename T>
 class Stream_Producer;
@@ -30,155 +64,139 @@ bool are_connected(Stream_Producer<T> & source, Stream_Consumer<T> & sink);
 template <typename T>
 class Stream_Producer
 {
+    using Consumer_Ptr = Detail::Stream_Consumer_Data_Ptr<T>;
+
 public:
     friend class Stream_Consumer<T>;
     friend void connect<T>(Stream_Producer<T> & source, Stream_Consumer<T> & sink);
     friend void disconnect<T>(Stream_Producer<T> & source, Stream_Consumer<T> & sink);
     friend bool are_connected<T>(Stream_Producer<T> & source, Stream_Consumer<T> & sink);
 
+    // Blocking
+
+    Stream_Producer(): d(new Detail::Stream_Producer_Data<T>) {}
+
+    // Blocking
+
     ~Stream_Producer()
     {
-        bool done = false;
-
-        while(!done)
-        {
-            lock_guard<mutex> guard(d_mutex);
-
-            auto it = d_sinks.begin();
-            while(it != d_sinks.end())
-            {
-                auto sink = *it;
-                if (!sink->d_mutex.try_lock())
-                    break;
-                sink->d_sources.remove(this);
-                sink->d_mutex.unlock();
-
-                it = d_sinks.erase(it);
-            }
-
-            done = d_sinks.empty();
-        }
+        d->sinks.for_each([&](const Consumer_Ptr & consumer){
+            consumer->sources.remove(d);
+        });
     }
+
+    // Lock-free
 
     void push(const T & val)
     {
-        lock_guard<mutex> guard(d_mutex);
-
-        for (auto sink : d_sinks)
-        {
-            sink->d_queue.push(val);
-        }
+        d->sinks.for_each([&](const Consumer_Ptr & consumer){
+            consumer->queue.push(val);
+        });
     }
+
+    // Lock-free
 
     bool has_connections()
     {
-        lock_guard<mutex> guard(d_mutex);
-        return !d_sinks.empty();
+        return !d->sinks.empty();
     }
 
 private:
-    mutex d_mutex;
-    list<Stream_Consumer<T>*> d_sinks;
+    Detail::Stream_Producer_Data_Ptr<T> d;
 };
 
 template <typename T>
 class Stream_Consumer
 {
+    using Producer_Ptr = Detail::Stream_Producer_Data_Ptr<T>;
+
 public:
     friend class Stream_Producer<T>;
     friend void connect<T>(Stream_Producer<T> & source, Stream_Consumer<T> & sink);
     friend void disconnect<T>(Stream_Producer<T> & source, Stream_Consumer<T> & sink);
     friend bool are_connected<T>(Stream_Producer<T> & source, Stream_Consumer<T> & sink);
 
-    Stream_Consumer(int capacity):
-        d_queue(capacity)
+    // Blocking
+
+    Stream_Consumer(int capacity): d(new Detail::Stream_Consumer_Data<T>(capacity))
     {
         if (capacity < 1)
             throw std::runtime_error("Invalid capacity.");
     }
 
+    // Blocking
+
     ~Stream_Consumer()
     {
-        bool done = false;
-
-        while(!done)
-        {
-            lock_guard<mutex> guard(d_mutex);
-
-            auto it = d_sources.begin();
-            while(it != d_sources.end())
-            {
-                auto src = *it;
-                if (!src->d_mutex.try_lock())
-                    break;
-                src->d_sinks.remove(this);
-                src->d_mutex.unlock();
-
-                it = d_sources.erase(it);
-            }
-
-            done = d_sources.empty();
-        }
+        d->sources.for_each([&](const Producer_Ptr & producer){
+            producer->sinks.remove(d);
+        });
     }
+
+    // Wait-free
 
     bool empty()
     {
-        return d_queue.empty();
+        return d->queue.empty();
     }
+
+    // Wait-free
 
     bool pop(T & v)
     {
-        return d_queue.pop(v);
+        return d->queue.pop(v);
     }
+
+    // Lock-free
 
     bool has_connections()
     {
-        lock_guard<mutex> guard(d_mutex);
-        return !d_sources.empty();
+        return !d->sources.empty();
     }
 
 private:
-    friend class Stream_Producer<T>;
-
-    mutex d_mutex;
-    list<Stream_Producer<T>*> d_sources;
-    MPSC_Queue<T> d_queue;
+    Detail::Stream_Consumer_Data_Ptr<T> d;
 };
+
+// Thread safe against:
+// - Itself
+// - All methods of Stream_Producer<T> and Stream_Consumer<T> other than the destructors.
+// Blocking.
+// Time O(N).
 
 template <typename T>
 inline
 void connect(Stream_Producer<T> & source, Stream_Consumer<T> & sink)
 {
-    lock_guard<mutex> guard1(source.d_mutex);
-    lock_guard<mutex> guard2(sink.d_mutex);
-
-    bool connected = find(source.d_sinks.begin(), source.d_sinks.end(), &sink) != source.d_sinks.end();
-    if (connected)
-        return;
-
-    source.d_sinks.push_back(&sink);
-    sink.d_sources.push_back(&source);
+    source.d->sinks.insert(sink.d);
+    sink.d->sources.insert(source.d);
 }
+
+// Thread safe against:
+// - Itself
+// - All methods of Stream_Producer<T> and Stream_Consumer<T> other than the destructors.
+// Blocking.
+// Time O(N).
 
 template <typename T>
 inline
 void disconnect(Stream_Producer<T> & source, Stream_Consumer<T> & sink)
 {
-    lock_guard<mutex> guard1(source.d_mutex);
-    lock_guard<mutex> guard2(sink.d_mutex);
-
-    source.d_sinks.remove(&sink);
-    sink.d_sources.remove(&source);
+    source.d->sinks.remove(sink.d);
+    sink.d->sources.remove(source.d);
 }
+
+// Thread safe against:
+// - Itself
+// - All methods of Stream_Producer<T> and Stream_Consumer<T> other than the destructors.
+// Blocking.
+// Time O(N).
 
 template <typename T>
 inline
 bool are_connected(Stream_Producer<T> & source, Stream_Consumer<T> & sink)
 {
-    lock_guard<mutex> guard1(source.d_mutex);
-    lock_guard<mutex> guard2(sink.d_mutex);
-
-    return find(source.d_sinks.begin(), source.d_sinks.end(), &sink) != source.d_sinks.end();
+    return source.d->sinks.contains(sink.d);
 }
 
 }
