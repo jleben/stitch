fc7c6e7a42eb6d0d647edaba7d59ed92fbc47203
Jakob Leben <jakob.leben@gmail.com>
2018-01-03 10:15:12 -0700

    Docs: Timer

--- stitch/linux/timer.h
+++ stitch/linux/timer.h
@@ -13,16 +13,37 @@ public:
     Timer();
     ~Timer();
 
+    /**
+     * @brief Starts the timer.
+     *
+     * If `repeated` is true, the \ref event is activated repeatedly
+     * with a period equal to `duration`.
+     * Otherwise, the event is activated only a single time
+     * after `duration`, and then the timer is stopped.
+     *
+     * When calling this method while the timer is already running,
+     * the timer is restarted with the new parameters.
+     */
     template<class Rep, class Period>
-    void start(const std::chrono::duration<Rep,Period> & dur, bool repeated = false)
+    void start(const std::chrono::duration<Rep,Period> & duration, bool repeated = false)
     {
-        setInterval(to_timespec(dur), repeated);
+        setInterval(to_timespec(duration), repeated);
     }
+
+    /**
+     * @brief Stops the timer.
+     *
+     * After stopping the timer, the \ref event will not be activated
+     * unless the timer is started again.
+     */
 
     void stop();
 
     void wait() { Stitch::wait(event()); }
 
+    /**
+     * @brief Returns a momentary event activated every timer period.
+     */
     Event event();
 
 private:
