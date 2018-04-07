b02a8533c8904bde7ad9fba2fe115c76164ef84e
Jakob Leben <jakob.leben@gmail.com>
2018-04-07 01:27:49 -0700

    Hazar pointers: More tests

--- tests/test_hazard_pointers.cpp
+++ tests/test_hazard_pointers.cpp
@@ -3,17 +3,26 @@
 
 #include <string>
 #include <thread>
+#include <vector>
+#include <chrono>
 
 using namespace Stitch;
 using namespace Testing;
 
 using namespace std;
 
-bool test_allocation()
+using Detail::Hazard_Pointers;
+using Detail::Hazard_Pointer;
+
+bool test_stress_allocation()
 {
     Test test;
 
-    int repetitions = 10000;
+    using clock = chrono::steady_clock;
+
+    auto start = clock::now();
+
+    chrono::seconds duration(1);
 
     auto thread_func = [&](int)
     {
@@ -22,40 +31,47 @@ bool test_allocation()
         int three = 3;
         int four = 4;
         bool ok = true;
+        bool count_ok = true;
 
-        int count = 0;
-
-        for (int i = 0; i < repetitions; ++i)
+        while(clock::now() - start < duration)
         {
-            auto & p1 = Detail::Hazard_Pointers::acquire<int>();
-            auto & p2 = Detail::Hazard_Pointers::acquire<int>();
-            auto & p3 = Detail::Hazard_Pointers::acquire<int>();
-            auto & p4 = Detail::Hazard_Pointers::acquire<int>();
+            int sum = 0;
+            int rep_count = 1000;
 
-            // Assign something
-            p1.pointer = &one;
-            p2.pointer = &two;
-            p3.pointer = &three;
-            p4.pointer = &four;
+            for (int i = 0; i < rep_count; ++i)
+            {
+                auto & p1 = Detail::Hazard_Pointers::acquire<int>();
+                auto & p2 = Detail::Hazard_Pointers::acquire<int>();
+                auto & p3 = Detail::Hazard_Pointers::acquire<int>();
+                auto & p4 = Detail::Hazard_Pointers::acquire<int>();
 
-            ok &= *p1.pointer.load() == 1;
-            ok &= *p2.pointer.load() == 2;
-            ok &= *p3.pointer.load() == 3;
-            ok &= *p4.pointer.load() == 4;
+                // Assign something
+                p1.pointer = &one;
+                p2.pointer = &two;
+                p3.pointer = &three;
+                p4.pointer = &four;
 
-            count += *p1.pointer.load();
-            count += *p2.pointer.load();
-            count += *p3.pointer.load();
-            count += *p4.pointer.load();
+                ok &= *p1.pointer.load() == 1;
+                ok &= *p2.pointer.load() == 2;
+                ok &= *p3.pointer.load() == 3;
+                ok &= *p4.pointer.load() == 4;
 
-            p1.release();
-            p2.release();
-            p3.release();
-            p4.release();
+                sum += *p1.pointer.load();
+                sum += *p2.pointer.load();
+                sum += *p3.pointer.load();
+                sum += *p4.pointer.load();
+
+                p1.release();
+                p2.release();
+                p3.release();
+                p4.release();
+            }
+
+            count_ok &= sum == 10 * rep_count;
         }
 
         test.assert("Allocation consistent.", ok);
-        test.assert("Total count ok.", count == repetitions * 10);
+        test.assert("Sum consistent.", count_ok);
     };
 
     thread t1(thread_func, 1);
@@ -74,11 +90,14 @@ bool test_over_allocation()
     bool exception_thrown = false;
     int i;
 
+    vector<Hazard_Pointer<int>*> hps;
+
     try
     {
         for (i = 0; i < Detail::Hazard_Pointers::H + 1; ++i)
         {
-            Detail::Hazard_Pointers::acquire<int>();
+            auto & hp = Detail::Hazard_Pointers::acquire<int>();
+            hps.push_back(&hp);
         }
     }
     catch (std::runtime_error&)
@@ -90,13 +109,84 @@ bool test_over_allocation()
     test.assert("Exception was thrown at index " + to_string(i),
                 i == Detail::Hazard_Pointers::H);
 
+    for (auto * hp : hps)
+    {
+        hp->release();
+    }
+
+    return test.success();
+}
+
+bool test_reclamation()
+{
+    using Detail::Hazard_Pointers;
+    using Detail::Hazard_Pointer;
+
+    Test test;
+
+    static atomic<int> created_count { 0 };
+    static atomic<int> deleted_count { 0 };
+
+    struct Element
+    {
+        Element() { created_count.fetch_add(1); }
+        Element(const Element &): Element() {}
+        ~Element() { deleted_count.fetch_add(1); }
+    };
+
+    int repetitions = 100;
+
+    for (int rep = 0; rep < repetitions; ++rep)
+    {
+        vector<Hazard_Pointer<Element>*> hps;
+
+        created_count = 0;
+        deleted_count = 0;
+
+        for (int i = 0; i < 5; ++i)
+        {
+            Element * p = new Element;
+            auto & hp = Hazard_Pointers::acquire<Element>();
+            hp.pointer = p;
+            hps.emplace_back(&hp);
+
+            Hazard_Pointers::reclaim(p);
+        }
+
+        for (int i = 0; i < Hazard_Pointers::H && deleted_count == 0; ++i)
+        {
+            Element * p = new Element;
+            Hazard_Pointers::reclaim(p);
+        }
+
+        test.assert_critical("Something was deleted.", deleted_count > 0);
+        test.assert_critical("Hazardous pointers were not deleted.", deleted_count == created_count - 5);
+
+        int last_deleted_count = deleted_count;
+
+        for (auto * hp : hps)
+        {
+            hp->pointer = nullptr;
+            hp->release();
+        }
+
+        for (int i = 0; i < Hazard_Pointers::H && deleted_count == last_deleted_count; ++i)
+        {
+            Element * p = new Element;
+            Hazard_Pointers::reclaim(p);
+        }
+
+        test.assert_critical("Everything was deleted.", deleted_count == created_count);
+    }
+
     return test.success();
 }
 
 Test_Set hazard_pointers_tests()
 {
     return {
-        { "allocation", test_allocation },
+        { "stress-allocation", test_stress_allocation },
+        { "reclamation", test_reclamation },
         { "over-allocation", test_over_allocation },
     };
 }
